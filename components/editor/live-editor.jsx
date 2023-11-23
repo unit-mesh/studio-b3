807d88645b8b13d51ac87cb32747e58c9259f1e5
Phodal Huang <h@phodal.com>
2023-11-23 15:58:13 +0800

    refactor: add design for ui components

--- components/editor/live-editor.jsx
+++ components/editor/live-editor.jsx
@@ -6,8 +6,6 @@ import ListItem from '@tiptap/extension-list-item'
 import TextStyle from '@tiptap/extension-text-style'
 import StarterKit from '@tiptap/starter-kit'
 
-import * as Accordion from '@radix-ui/react-accordion'
-
 import MarkdownIt from 'markdown-it'
 
 import { MenuBar } from './menu-bar'
@@ -16,7 +14,7 @@ import { createSlashExtension } from './intelli/slash-extension'
 import { CommandFunctions } from './action/command-functions'
 import { createAiBlock } from './intelli/ai-block-extension'
 import TrackChangeExtension from './diff/track-change-extension'
-import { ChevronDownIcon } from '@radix-ui/react-icons'
+import { Sidebar } from './sidebar'
 
 const md = new MarkdownIt()
 
@@ -110,60 +108,5 @@ const LiveEditor = () => {
     </div>
   )
 }
-
-const Sidebar = () => {
-  return <aside className={'fixed top-0 right-0 z-40 w-128 h-screen'}
-                aria-label="Sidebar">
-    <div className={'h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800'}>
-      <Accordion.Root className={'AccordionRoot'} type="multiple" defaultValue={['item-1', 'item-2', 'item-3']}>
-        <Accordion.Item className={'AccordionItem'} value="item-1">
-          <AccordionTrigger>Grammarly</AccordionTrigger>
-          <AccordionContent>
-            TODO: use some model to check grammar
-          </AccordionContent>
-        </Accordion.Item>
-
-        <Accordion.Item className={'AccordionItem'} value="item-2">
-          <AccordionTrigger>Text Prediction</AccordionTrigger>
-          <AccordionContent>
-            TODO: use <a href="https://github.com/unit-mesh/edge-infer">EdgeInference</a> to predict text
-          </AccordionContent>
-        </Accordion.Item>
-
-        <Accordion.Item className={'AccordionItem'} value="item-3">
-          <AccordionTrigger>Similarity</AccordionTrigger>
-          <Accordion.Content className={'AccordionContent'}>
-            <div className={'AccordionContentText'}>
-              TODO: use <a href="https://github.com/unit-mesh/edge-infer">EdgeInference</a> to calculate similarity
-            </div>
-          </Accordion.Content>
-        </Accordion.Item>
-      </Accordion.Root>
-    </div>
-  </aside>
-}
-
-const AccordionTrigger = React.forwardRef(({ children, className, ...props }, forwardedRef) => (
-  <Accordion.Header className="AccordionHeader">
-    <Accordion.Trigger
-      className={'AccordionTrigger'}
-      {...props}
-      ref={forwardedRef}
-    >
-      {children}
-      <ChevronDownIcon className="AccordionChevron" aria-hidden/>
-    </Accordion.Trigger>
-  </Accordion.Header>
-))
-
-const AccordionContent = React.forwardRef(({ children, className, ...props }, forwardedRef) => (
-  <Accordion.Content
-    className={'AccordionContent'}
-    {...props}
-    ref={forwardedRef}
-  >
-    <div className="AccordionContentText">{children}</div>
-  </Accordion.Content>
-))
 
 export default LiveEditor
---
+++ components/editor/sidebar.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import * as Accordion from '@radix-ui/react-accordion'
+import { ChevronDownIcon } from '@radix-ui/react-icons'
+
+export const Sidebar = ({ editor }) => {
+  return <aside className={'fixed top-0 right-0 z-40 w-128 h-screen'}
+                aria-label="Sidebar">
+    <div className={'h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800'}>
+      <Accordion.Root className={'AccordionRoot'} type="multiple" defaultValue={['item-1', 'item-2', 'item-3']}>
+        <Accordion.Item className={'AccordionItem'} value="item-1">
+          <AccordionTrigger>Grammarly</AccordionTrigger>
+          <AccordionContent>
+            TODO: use some model to check grammar
+          </AccordionContent>
+        </Accordion.Item>
+
+        <Accordion.Item className={'AccordionItem'} value="item-2">
+          <AccordionTrigger>Text Prediction</AccordionTrigger>
+          <AccordionContent>
+            TODO: use <a href="https://github.com/unit-mesh/edge-infer">EdgeInference</a> to predict text
+          </AccordionContent>
+        </Accordion.Item>
+
+        <Accordion.Item className={'AccordionItem'} value="item-3">
+          <AccordionTrigger>Similarity</AccordionTrigger>
+          <Accordion.Content className={'AccordionContent'}>
+            <div className={'AccordionContentText'}>
+              TODO: use <a href="https://github.com/unit-mesh/edge-infer">EdgeInference</a> to calculate similarity
+            </div>
+          </Accordion.Content>
+        </Accordion.Item>
+      </Accordion.Root>
+    </div>
+  </aside>
+}
+
+const AccordionTrigger = React.forwardRef(({ children, className, ...props }, forwardedRef) => (
+  <Accordion.Header className="AccordionHeader">
+    <Accordion.Trigger
+      className={'AccordionTrigger'}
+      {...props}
+      ref={forwardedRef}
+    >
+      {children}
+      <ChevronDownIcon className="AccordionChevron" aria-hidden/>
+    </Accordion.Trigger>
+  </Accordion.Header>
+))
+
+const AccordionContent = React.forwardRef(({ children, className, ...props }, forwardedRef) => (
+  <Accordion.Content
+    className={'AccordionContent'}
+    {...props}
+    ref={forwardedRef}
+  >
+    <div className="AccordionContentText">{children}</div>
+  </Accordion.Content>
+))
