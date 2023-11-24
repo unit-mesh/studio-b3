28d739112b0b9689d1c2e15b1b7ca90262683ecf
Phodal Huang <h@phodal.com>
2023-11-24 09:40:12 +0800

    docs: add links

--- src/components/editor/sidebar.tsx
+++ src/components/editor/sidebar.tsx
@@ -5,7 +5,24 @@ import { ChevronDownIcon } from '@radix-ui/react-icons'
 export const Sidebar: React.FC<any> = ({ editor }) => {
 	return <aside className={'fixed top-0 right-0 z-40 w-128 h-screen'} aria-label="Sidebar">
 		<div className={'h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800'}>
-			<Accordion.Root className={'AccordionRoot'} type="multiple" defaultValue={['item-1', 'item-2', 'item-3']}>
+			<Accordion.Root className={'AccordionRoot'} type="multiple" defaultValue={['item-1', 'item-2', 'item-3', 'item-4']}>
+				<Accordion.Item className={'AccordionItem'} value="item-6">
+					<AccordionTrigger>Custom Related Resource Link</AccordionTrigger>
+					<AccordionContent>
+						<input className={'w-full bg-white'}/>
+					</AccordionContent>
+				</Accordion.Item>
+
+				<Accordion.Item className={'AccordionItem'} value="item-5">
+					<AccordionTrigger>PreContext</AccordionTrigger>
+					<AccordionContent>
+						<textarea
+							className={'w-full'}
+							placeholder={'context about this article'}
+						/>
+					</AccordionContent>
+				</Accordion.Item>
+
 				<Accordion.Item className={'AccordionItem'} value="item-1">
 					<AccordionTrigger>Grammarly</AccordionTrigger>
 					<AccordionContent>
@@ -25,6 +42,15 @@ export const Sidebar: React.FC<any> = ({ editor }) => {
 					<Accordion.Content className={'AccordionContent'}>
 						<div className={'AccordionContentText'}>
 							TODO: use <a href="https://github.com/unit-mesh/edge-infer">EdgeInference</a> to calculate similarity
+						</div>
+					</Accordion.Content>
+				</Accordion.Item>
+
+				<Accordion.Item className={'AccordionItem'} value="item-3">
+					<AccordionTrigger>Web Search</AccordionTrigger>
+					<Accordion.Content className={'AccordionContent'}>
+						<div className={'AccordionContentText'}>
+							TODO
 						</div>
 					</Accordion.Content>
 				</Accordion.Item>
