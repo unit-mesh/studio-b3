303e01bd681daf882172bfefdf33f25a255a5345
Phodal Huang <h@phodal.com>
2023-11-24 10:02:56 +0800

    feat: init for language changeed

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,5 +1,6 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 // the translations
 // (tip move them in a JSON file and import them,
@@ -29,8 +30,11 @@ const resources = {
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
   .init({
     resources,
+    fallbackLng: 'en',
+    debug: true,
     // lng: 'zh', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
@@ -38,6 +42,9 @@ i18n
     interpolation: {
       escapeValue: false // react already safes from xss
     }
+  }, (err, t) => {
+    if (err) return console.log('something went wrong loading', err)
+    console.log('i18n loaded successfully')
   })
 
 export default i18n
