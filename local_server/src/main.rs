dda76e5879b0861616be4334b2df6c8e63bbd150
Phodal Huang <h@phodal.com>
2023-11-27 18:01:17 +0800

    feat(server): init for database

--- local_server/src/main.rs
+++ local_server/src/main.rs
@@ -1,6 +1,9 @@
 pub mod scraper;
 
+use std::ops::Deref;
+use std::sync::Arc;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use sqlx::{Executor, Pool, Sqlite, SqlitePool};
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -18,6 +21,10 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
+    let sql = Arc::new(initialize().await);
+    sql.deref().execute("CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY, name TEXT NOT NULL)").await.unwrap();
+
     HttpServer::new(|| {
         App::new()
             .service(hello)
@@ -27,4 +34,12 @@ async fn main() -> std::io::Result<()> {
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
+}
+
+#[tracing::instrument(skip_all)]
+pub async fn initialize() -> Pool<Sqlite> {
+    let url = format!("sqlite://./3b.db?mode=rwc");
+
+    let pool = SqlitePool::connect(&*url);
+    return pool.await.unwrap();
 }
