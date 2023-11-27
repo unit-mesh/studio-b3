00378c9402db0393dc03b4b76fccf7da0dd4e092
Phodal Huang <h@phodal.com>
2023-11-27 20:40:44 +0800

    refactor: extract for app

--- local_server/src/main.rs
+++ local_server/src/main.rs
@@ -1,39 +1,22 @@
 pub mod scraper;
+mod create_app;
 
 use std::ops::Deref;
 use std::sync::Arc;
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{HttpServer, Responder};
 use sqlx::{Executor, Pool, Sqlite, SqlitePool};
-
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
     let sql = Arc::new(initialize().await);
     sql.deref().execute("CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY, name TEXT NOT NULL)").await.unwrap();
 
     HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+        create_app::create_app()
     })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
 
 #[tracing::instrument(skip_all)]
