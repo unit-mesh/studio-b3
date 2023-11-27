b789f3ccd945f31c172aa218cf1c98cce7cdc29b
Phodal Huang <h@phodal.com>
2023-11-27 10:12:45 +0800

    fix: fix ts relative import issue

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,29 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
-  ],
-  framework: {
-    name: "@storybook/nextjs",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
+	stories: [
+		"../src/**/*.mdx",
+		"../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+	],
+	addons: [
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@storybook/addon-onboarding",
+		"@storybook/addon-interactions",
+	],
+	framework: {
+		name: "@storybook/nextjs",
+		options: {},
+	},
+	docs: {
+		autodocs: "tag",
+	},
+	webpackFinal: async (config, { configType }) => {
+		// @ts-ignore
+		config.resolve.plugins = [new TsconfigPathsPlugin()];
+		return config;
+	}
 };
 export default config;
