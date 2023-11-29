d8b0f5adbde71d798ac78cbb3b3c8bf603b6e850
CGQAQ <m.jason.liu@outlook.com>
2023-11-29 09:43:59 +0800

    hotfix: whitespace and markers are now kept

--- local_server/src/doc_split/office_splitter.rs
+++ local_server/src/doc_split/office_splitter.rs
@@ -19,7 +19,7 @@ impl Splitter for OfficeSplitter {
 
         let buf_size = options.chunk_size * 4;
         let mut buffer = String::with_capacity(buf_size);
-        for word in document.unicode_words() {
+        for word in document.split_word_bounds() {
             if buffer.len() + word.len() <= buf_size {
                 buffer.push_str(word);
             } else {
