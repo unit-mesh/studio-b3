0bb5821fd20194389367aa993441da3493d0a211
Phodal Huang <h@phodal.com>
2023-12-03 15:15:49 +0800

    fix: fix open ai parameters issues

--- editor/src/app/api/completion/openai/route.ts
+++ editor/src/app/api/completion/openai/route.ts
@@ -13,12 +13,17 @@ export async function POST(req: Request) {
 	const { prompt } = await req.json();
 
 	// Ask OpenAI for a streaming completion given the prompt
-	const response = await openai.completions.create({
-		model: 'text-davinci-003',
+	const response = await openai.chat.completions.create({
+		model: 'gpt-3.5-turbo',
 		stream: true,
 		temperature: 0.6,
-		max_tokens: 300,
-		prompt: prompt,
+		max_tokens: 1200,
+		messages: [
+			{
+				role: 'user',
+				content: prompt,
+			},
+		]
 	});
 	// Convert the response into a friendly text-stream
 	const stream = OpenAIStream(response);
