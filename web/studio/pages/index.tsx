193861d1f47d9542e734f95232996d6c528664f9
Phodal Huang <h@phodal.com>
2023-12-11 21:22:34 +0800

    refactor: clean styles

--- web/studio/pages/index.tsx
+++ web/studio/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 
+import "../styles/editor-styles.css"
 import styles from '../styles/Home.module.css'
 import {LiveEditor} from '@studio-b3/web-core'
 import '@/i18n/i18n';
