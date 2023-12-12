39b52466ca074a6f28d81e1d3a5f19c584b7c46e
Phodal Huang <h@phodal.com>
2023-12-12 21:33:10 +0800

    styles: fix lint

--- web/studio/app/api/completion/mock/route.ts
+++ web/studio/app/api/completion/mock/route.ts
@@ -1,24 +1,23 @@
 import { OpenAIStream, StreamingTextResponse } from 'ai';
-// import { Response } from 'openai/src/core';
 
 export async function POST(req: Request) {
   const responseData = {
-    "choices": [
+    'choices': [
       {
-        "message": {
-          "content": "Hello, world",
-          "role": "assistant"
+        'message': {
+          'content': 'Hello, world',
+          'role': 'assistant'
         }
       }
     ],
-    "created": 1677664795,
-    "id": "chatcmpl-7QyqpwdfhqwajicIEznoc6Q47XAyW",
-    "model": "gpt-3.5-turbo-0613",
-    "object": "chat.completion",
-  }
+    'created': 1677664795,
+    'id': 'chatcmpl-7QyqpwdfhqwajicIEznoc6Q47XAyW',
+    'model': 'gpt-3.5-turbo-0613',
+    'object': 'chat.completion'
+  };
 
   const response = new Response(JSON.stringify(responseData), {
-    headers: { 'Content-Type': 'application/json' },
+    headers: { 'Content-Type': 'application/json' }
   });
 
   const stream = OpenAIStream(response);
