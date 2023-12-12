6802f8e77551dca791dc6955349624ee812d8619
Phodal Huang <h@phodal.com>
2023-12-12 14:12:47 +0800

    fix: fix lint

--- web/studio/app/api/completion/mock/route.ts
+++ web/studio/app/api/completion/mock/route.ts
@@ -2,18 +2,15 @@ import { OpenAIStream, StreamingTextResponse } from 'ai';
 
 
 export async function POST(req: Request) {
-	const { prompt } = await req.json();
+  const response = new ReadableStream({
+    start(controller) {
+      controller.enqueue('Hello World, this is from mock api');
+      controller.close();
+    }
+  });
 
-	// mock response to return text stream , "Hello World" is the response
-	const response = new ReadableStream({
-		start(controller) {
-			controller.enqueue("Hello World, this is from mock api");
-			controller.close();
-		}
-	});
-
-	// Convert the response into a friendly text-stream
-	const stream = OpenAIStream(new Response(response));
-	// Respond with the stream
-	return new StreamingTextResponse(stream);
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(new Response(response));
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
 }
